89a4741c951db7b7dffdfbe050233814a5357b8f
Brianna Tanusi <btanusi@gmail.com>
2020-10-27 14:38:37 -0700

    added roshambo functionality

---
+++ main.js
@@ -0,0 +1,63 @@
+class Rock{
+    static beats(){
+        return Scissors
+    }
+}
+
+class Paper{
+    static beats(){
+        return Rock
+    }
+}
+
+class Scissors{
+    static beats(){
+        return Paper
+    }
+}
+
+class Game{
+    constructor(playerPlay){
+        this.playerPlayStr = playerPlay
+        this.playerPlay = this.type(playerPlay)
+        this.computerPlayStr = this.randomizedComputerPlay()
+        this.computerPlay = this.type(this.computerPlayStr)
+    }
+
+    randomizedComputerPlay(){
+        var randomPlay = Math.ceil(Math.random()*3);
+        if (randomPlay === 1){
+            return this.computerPlayStr = 'rock'
+        } else if (randomPlay === 2){
+            return this.computerPlayStr = 'paper'
+        } else {
+            return this.computerPlayStr = 'scissors'
+        }
+    }
+
+    type(playStr){
+        if (playStr === 'rock'){
+            return Rock
+        } else if (playStr === 'paper'){
+            return Paper 
+        } else {
+            return Scissors
+        }
+    }
+
+    compete(){
+        var winner = '';
+        if (this.playerPlay === this.computerPlay){
+            winner = 'Player and Computer tie.'
+        } else if (this.playerPlay.beats() === this.computerPlay){
+            winner = `Player wins.`
+        } else {
+            winner = `Computer wins.`
+        }
+        return `Playing a game of Roshambo against the computer.\nPlayer plays ${this.playerPlayStr}!\nComputer plays ${this.computerPlayStr}!\n~${winner}~`
+    }
+}
+
+var playerPlay = process.argv[2].substring(7)
+var myGame = new Game(playerPlay)
+console.log(myGame.compete())
